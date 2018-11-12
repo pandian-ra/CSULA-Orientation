c739ac19ec737dca4c568b16ad3ceb37ed58b5ef
Pandian <pandian-ra@users.noreply.github.com>
2018-11-12 08:17:43 -0800

    Initial Commit

---
+++ CSULA Orientation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSULA Orientation
+//
+//  Created by Pandian Raja on 11/12/18.
+//  Copyright © 2018 Pandian Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
